c88b989d8ee406622e9a8972e85196fc6d4f20e2
anilknanda <122780943+anilknanda@users.noreply.github.com>
2023-01-17 17:15:03 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] s){
         System.out.println("Hello oracle world");
         for (int i=0;i<=10;i++){
-            System.out.println("RESULT " +i);
+            System.out.println("RESULT :" +i);
         }
     }
 }
